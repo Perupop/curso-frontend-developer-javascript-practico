e19a8f794d1a7c64c0a9e8ca5f4b09f5be24e1db
Pedro <pedroantoniopina25@gmail.com>
2023-02-19 17:56:55 -0400

    se agrgo el detalle de mis ordenes hechas

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuhamburgicon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingcart = document.querySelector('.navbar-shopping-cart');
+const productdetail = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuhamburgicon.addEventListener('click', togglemobilemenu);
+shoppingcart.addEventListener('click', toggleshoppingcart)
 
 function toggleDesktopMenu() {
+    const detailprodc = productdetail.classList.contains('inactive');
+    
+    if(!detailprodc){
+        productdetail.classList.add("inactive");
+
+    }
+    
+    
     desktopMenu.classList.toggle('inactive');
+}
+
+function togglemobilemenu() {
+    const detailprodc = productdetail.classList.contains('inactive');
+    
+    if(!detailprodc){
+        productdetail.classList.add("inactive");
+
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleshoppingcart() {
+    const menumovil = mobileMenu.classList.contains('inactive');
+    const menudesktop = desktopMenu.classList.contains('inactive');
+    if(!menumovil){
+        mobileMenu.classList.add("inactive");
+
+    }
+    
+    productdetail.classList.toggle('inactive');
 }
